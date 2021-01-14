a090fd3cadddc15d78e86ea4fbb5b0c46ecbd2cf
Yecheng Fu <cofyc.jackson@gmail.com>
2021-01-14 19:07:46 +0800

    add advanced access control recipe

--- hack/run.sh
+++ hack/run.sh
@@ -11,6 +11,10 @@ path=$1
 
 docker rm -f hack-nginx &>/dev/null || true
 
+IMAGE=nginx
+IMAGE=k8s.gcr.io/ingress-nginx/controller:v0.41.0
+
 docker run --rm --name hack-nginx -p 8080:80 \
    -v $(pwd)/$path:/etc/nginx/nginx.conf:ro \
-    nginx nginx -g 'daemon off;'
+   $IMAGE \
+   nginx -g 'daemon off;'
