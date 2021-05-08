8b8d1a25bf1174c5a35f8ce6fea8f8340a614c82
Yecheng Fu <cofyc.jackson@gmail.com>
2021-05-08 13:07:14 +0800

    https example

--- hack/run.sh
+++ hack/run.sh
@@ -16,6 +16,7 @@ IMAGE=k8s.gcr.io/ingress-nginx/controller:v0.41.2
 
 docker run --rm --name hack-nginx -p 8080:80 \
    --net host \
+   -v $(pwd)/ssl:/etc/nginx/ssl \
    -v $(pwd)/$path:/etc/nginx/nginx.conf:ro \
    $IMAGE \
    nginx -g 'daemon off;'
